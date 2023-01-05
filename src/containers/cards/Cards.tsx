b39dfc71ff841601f2498f8b57d80e8f5784079a
amirHosseinKhademii <sourena2575@gmail.com>
2023-01-05 23:08:20 +0100

    Add todo cards fragment

--- src/containers/cards/Cards.tsx
+++ src/containers/cards/Cards.tsx
@@ -1,13 +1,21 @@
-export const Cards = () => {
-  // const {
-  //   data,
-  //   loadNext,
-  //   loadPrevious,
-  //   hasNext,
-  //   hasPrevious,
-  //   isLoadingNext,
-  //   isLoadingPrevious,
-  //   refetch, // For refetching connection
-  // } = usePaginationFragment<CardsPaginationQuery, any>(CardsFragment);
-  return <div>Cards</div>;
+import { TodoCardsFragment } from "containers/todo/graphql/Todo.queries";
+import { TodoCardsFragment$key } from "containers/todo/graphql/__generated__/TodoCardsFragment.graphql";
+import { TodoQuery$data } from "containers/todo/graphql/__generated__/TodoQuery.graphql";
+import { useFragment, usePaginationFragment } from "react-relay";
+
+export const Cards = ({ todo }: { todo: TodoCardsFragment$key }) => {
+  const { cards } = useFragment(TodoCardsFragment, todo);
+
+  return (
+    <ul className=" border border-cyan-500 bg-cyan-200 rounded p-4 shadow-md flex flex-col space-y-2 ">
+      {cards.edges?.map((card) => (
+        <li
+          className="bg-amber-200 border border-amber-300 rounded p-2"
+          key={card.node?.id}
+        >
+          {card.node?.title}
+        </li>
+      ))}
+    </ul>
+  );
 };
--- src/containers/todo/Todos.tsx
+++ src/containers/todo/Todos.tsx
@@ -6,22 +6,17 @@ import {
 } from "react-relay";
 import { TodoQuery as TTodoQuery } from "containers/todo/graphql/__generated__/TodoQuery.graphql";
 import { TodoQuery } from "containers/todo/graphql/Todo.queries";
+import { Cards } from "containers/cards/Cards";
 interface TTodo {
   initialQueryRef: PreloadedQuery<TTodoQuery>;
 }
 export const Todo = ({ initialQueryRef }: TTodo) => {
-  const data = usePreloadedQuery<TTodoQuery>(TodoQuery, initialQueryRef);
-  console.log(data);
+  const { todo } = usePreloadedQuery<TTodoQuery>(TodoQuery, initialQueryRef);
 
   return (
-    <div>
-      todo
-      {/* {data.todos.edges?.map((todo) => (
-        <div id={todo.node?.id}>
-          {todo.node?.title}
-          <Cards />
-        </div>
-      ))} */}
+    <div className="mx-auto max-w-lg rounded border border-gray-300 my-40 p-4 shadow-lg">
+      <p className="text-lg pb-4 text-center"> {todo.title}</p>
+      <Cards {...{ todo }} />
     </div>
   );
 };
